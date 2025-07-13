c98b2f8f0fa913577ae7a90cd3b50a128a5a50ee
Jeff Hägerman <jeff@hagerman.io>
2025-07-13 15:35:42 +0200

    refactor: simplify apt.sh script by removing redundant paths

--- script/apt.sh
+++ script/apt.sh
@@ -4,17 +4,19 @@ set -e
 
 cp ./dist/*.deb ./apt/
 
+cd ./apt
+
 # Packages & Packages.gz
-dpkg-scanpackages --multiversion . > ./apt/Packages
+dpkg-scanpackages --multiversion . > Packages
 
 # change Filename to url
-sed -i "s|./hello-cli|https://github.com/MrJeffLarry/hello-cli/releases/download/$VERSION/hello-cli|" ./apt/Packages
+sed -i "s|./hello-cli|https://github.com/MrJeffLarry/hello-cli/releases/download/$VERSION/hello-cli|" Packages
 
-gzip -k -f ./apt/Packages
+gzip -k -f Packages
 
 # Release, Release.gpg & InRelease
-apt-ftparchive release ./apt/ > ./apt/Release
-gpg --default-key "$GPG_FINGERPRINT" -abs -o - ./apt/Release > ./apt/Release.gpg
-gpg --default-key "$GPG_FINGERPRINT" --clearsign -o - ./apt/Release > ./apt/InRelease
+apt-ftparchive release  > Release
+gpg --default-key "$GPG_FINGERPRINT" -abs -o - Release > Release.gpg
+gpg --default-key "$GPG_FINGERPRINT" --clearsign -o - Release > InRelease
 
-rm ./apt/*.deb
+rm *.deb
