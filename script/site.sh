dd5a6cc646031b22a3683af3fadae3ccdd616917
Jeff Hägerman <jeff@hagerman.io>
2025-07-21 16:17:20 +0200

    fix: update input directory path for markdown-to-pdf action and adjust site.sh for macOS compatibility

--- script/site.sh
+++ script/site.sh
@@ -6,8 +6,14 @@ mkdir -p out
 mkdir -p site
 
 cp README.md out/index.md
-sed -i "s|./docs/|./|g" out/index.md
-sed -i "s|.md|.html|g" out/index.md
-cp -r docs/ out/
+# if os is macOs
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s|./docs/|./|g" out/index.md
+    sed -i '' "s|.md|.html|g" out/index.md
+else
+    sed -i "s|./docs/|./|g" out/index.md
+    sed -i "s|.md|.html|g" out/index.md
+fi
+cp docs/* out/
 
 cp -r apt site/apt
